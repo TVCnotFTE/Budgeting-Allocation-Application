0cb7f6c8fc65bc26bdfc814686532e27cceb8aad
TVCnotFTE <d.steen712@gmail.com>
2024-02-22 22:13:28 -0500

    At least I added one alert

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,13 +3,21 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
+//    const { totalExpenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
-    }
+        if (newBudget > 20000) {
+            alert("Do I look like I'm made out of money?!?!?!  Budget cannot exceed $20,000.  Please pick a lower number.");
+//        } else if (newBudget < totalExpenses ) {
+//            alert("Woah, rockstar!  You already spent that!  Please raise your budget.");
+        } else {
+            return;
+        }
+    }; 
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: £</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
